0489d5502d6510159ffe12498262af737632f227
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-02-25 11:50:07 -0500

    first commit

---
+++ src/com/dee/Main.java
@@ -0,0 +1,22 @@
+package com.dee;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        String myString = "abcdefg";
+//        int index = 0;
+
+//        while (index < myString.length()) {
+//            char c = myString.charAt(index);
+//            System.out.println(c);
+//            index++;
+//        }
+//        System.out.println(" ");
+
+
+
+    }
+}
+
+
